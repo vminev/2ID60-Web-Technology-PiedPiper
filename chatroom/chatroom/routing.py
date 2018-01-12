d4234f01703c9485c07eb4602134109081d076cb
shunobaka <vesselin_minev@abv.bg>
2018-01-12 14:58:31 +0100

    Added channels implementation for distributing messages

---
+++ chatroom/chatroom/routing.py
@@ -0,0 +1,8 @@
+from channels.routing import route
+from .connections import ws_add, ws_message, ws_disconnect
+
+channel_routing = [
+    route("websocket.connect", ws_add, path=r"^/chat/(?P<room_id>\d+)$"),
+    route("websocket.receive", ws_message, path=r"^/chat/(?P<room_id>\d+)$"),
+    route("websocket.disconnect", ws_disconnect, path=r"^/chat/(?P<room_id>\d+)$"),
+]
