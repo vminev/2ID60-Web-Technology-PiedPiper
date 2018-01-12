a470f2b971c1a691313147988a04e1e47413369f
s168945 <ggoncheva@gmail.com>
2018-01-12 14:37:44 +0100

    Connection updates
    con upd

--- chatroom/connection/admin.py
+++ chatroom/connection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+# Register your models here.
+class ConnectionAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Connection
+
+
+admin.site.register(Connection, ConnectionAdmin)
