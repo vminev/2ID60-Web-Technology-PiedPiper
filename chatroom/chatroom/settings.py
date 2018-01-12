a6eecb6774a21181466e2b3e258b7c7c824d07b2
s168945 <ggoncheva@gmail.com>
2018-01-12 14:03:40 +0100

    Added connections
    Connections

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'roomchat',
-    'userprofile'
+    'userprofile',
+    'connection',
+    'membership'
 ]
 
 MIDDLEWARE = [
--- chatroom/connection/models.py
+++ chatroom/connection/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from userprofile.models import UserProfile
+from django.db import models
+
+
+# Create your models here.
+class Connection(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    chatroom = models.ForeignKey('roomchat.RoomChat', on_delete=models.CASCADE)
