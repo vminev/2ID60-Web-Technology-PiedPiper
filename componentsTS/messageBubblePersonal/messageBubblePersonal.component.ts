f706f0b2172515072fc316ef8034bdf51eb8fd28
Inibua <tzvetan.zahariev@gmail.com>
2018-01-13 11:48:10 +0100

    ComponentsTS folder added with html and css files
    Most components need css from bootrap and accurate adjustments. HTML for chatRoom needs adjustment. Folders for routing and services need to be added.

---
+++ componentsTS/messageBubblePersonal/messageBubblePersonal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-messageBubblePersonal',
+  templateUrl: '../viewsHTML/messageBubblePersonal/messageBubblePersonal.component.html',
+  styleUrls: ['../viewsCSS/messageBubblePersonal/messageBubblePersonal.component.css']
+})
+
+export class MessageBubblePersonal {
+
+  constructor(){
+
+  }
+}
